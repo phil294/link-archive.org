ac1c1014968877aa6934a6112c84c4c11e7cf8e3
phi <philipwaritschlager@web.de>
2017-06-12 23:03:33 +0200

    return nur relevante Attrs

--- src/main/java/phil294/ls/api/product/SearchController.java
+++ src/main/java/phil294/ls/api/product/SearchController.java
@@ -71,10 +71,10 @@ public class SearchController
 				"SELECT p.id,p.user,p.name,p.description,p.picture,";
 		// all attributes in the pivot table, from which to filter & sort afterwards
 		// order matters: == view order on website: 1st sorters, 2nd showers, 3rd filters
-		Set<Integer> pivot_attrs = Stream.concat(sorters.keySet().stream(), Stream.concat(showers.stream(), filters.keySet().stream()))
+		Set<Integer> relevant_attrs = Stream.concat(sorters.keySet().stream(), Stream.concat(showers.stream(), filters.keySet().stream()))
 				.collect(Collectors.toSet());
 		Collection<String> pivot_snippets = new ArrayList<>();
-		for(int attr : pivot_attrs) {
+		for(int attr : relevant_attrs) {
 			pivot_snippets.add("MAX( CASE WHEN attribute = " + attr + " THEN value END ) as attr" + attr + " ");
 		}
 		queryString += String.join(", ", pivot_snippets);
@@ -95,7 +95,7 @@ public class SearchController
 		for(Map.Entry<Integer, SortingOrder> sorterEntry : sorters.entrySet()) {
 			queryString += "attr" + sorterEntry.getKey() + " " + sorterEntry.getValue().name() + " ";
 		}
-		// (SHOWERS): -> happens via pivot_attrs
+		// (SHOWERS): -> happens via relevant_attrs
 		Query query = entityManager.createNativeQuery(queryString);
 		i = 0;
 		for(String filterValue : filters.values()) {
@@ -114,13 +114,13 @@ public class SearchController
 			product.setDescription((String) o[++ i]);
 			product.setPicture((String) o[++ i]);
 			// product values
-			for(int attr : pivot_attrs) {
+			for(int attr : relevant_attrs) {
 				product.getProductData().put(attr, (String) o[++ i]);
 			}
 			products.add(product);
 		}
 		
-		Iterable<Attribute> attributes = attributeRepository.findAll();
+		Iterable<Attribute> attributes = attributeRepository.findAll(relevant_attrs);
 		SearchResponse response = new SearchResponse(products, attributes);
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
