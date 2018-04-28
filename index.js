5b49078f17dd1f65d4f863f5dd4bf029922a42e3
phi <philipwaritschlager@web.de>
2018-04-28 19:12:29 +0200

    + token login

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const nodemailer = require('nodemailer');
+const jwt = require('jwt-simple');
 
 const MONGO_URL = process.env.MONGO_URL || (() => { throw new Error('MONGO_URL is not set'); })();
 const PORT = process.env.PORT || (() => { throw new Error('PORT is not set'); })();
 const MAIL_SENDER_USER = process.env.MAIL_SENDER_USER || (() => { throw new Error('MAIL_SENDER_USER is not set'); })();
 const MAIL_SENDER_PASSWORD = process.env.MAIL_SENDER_PASSWORD || (() => { throw new Error('MAIL_SENDER_PASSWORD is not set'); })();
+const TOKEN_SECRET = process.env.TOKEN_SECRET || (() => { throw new Error('TOKEN_SECRET is not set'); })();
+const WEB_ROOT = process.env.WEB_ROOT || (() => { throw new Error('WEB_ROOT is not set'); })();
 
 mongoose.connect(MONGO_URL);
 
@@ -34,17 +37,33 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // fixme
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Token');
 
     next();
 });
 
 app.get('/requesttokenmail', (req, res) => {
-    sendMail(req.query.email, 'Your Login Mail - ls', 'hi <b>bruh</b>')
-        .then(res.end)
+    const now = Date.now() / 1000;
+    const token = jwt.encode({
+        iat: now,
+        // exp: (Date.now() / 1000) + 3600,
+        email: req.query.email,
+    }, TOKEN_SECRET);
+    const url = `${WEB_ROOT}/#/login?token=${token}`;
+    sendMail(req.query.email, 'Your Login Mail - ??', `
+    Hello, <br>
+    here is the link to log in to ??:<br>
+    <a href="${url}">${url}</a><br>
+    Bye`)
+        .then(() => res.end())
         .catch((error) => {
             res.status(500).send(error);
         });
+});
+
+app.get('/secure', (req, res) => {
+    const { email } = jwt.decode(req.headers.token, TOKEN_SECRET);
+    res.send(email);
 });
 
 app.listen(PORT, () => {
