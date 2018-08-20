2d23db6d9c5591b356d66b2699e317c659cca047
Philip Waritschlager <philipwaritschlager@web.de>
2018-08-20 07:54:12 +0200

    webpack: force environment variables

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,8 +2,5 @@ const merge = require('webpack-merge');
 const prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
-    NODE_ENV: '"development"',
-    API_ROOT: '"http://localhost:7070"',
-    GOOGLE_CLIENT_ID: '"299730229518-qa5kouesun90urfmuuguf7p89rs38a39.apps.googleusercontent.com"', // fixme wtf
-    FACEBOOK_APP_ID: '"231287710788406"',
+    NODE_ENV: '"development"'
 });
