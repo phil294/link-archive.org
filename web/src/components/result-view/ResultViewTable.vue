18c25fdf9896adce96bda6769d0233ab0208cea9
Philip Waritschlager <philipwaritschlager@web.de>
2018-11-10 02:04:50 +0100

    sorters frontend impl

--- web/src/components/result-view/ResultViewTable.vue
+++ web/src/components/result-view/ResultViewTable.vue
@@ -3,8 +3,17 @@
 		thead
 			tr
 				th
-				th v-for="attribute in attributes" -key=attribute.name
-					| {{ attribute.name }}
+				th v-for="attribute in attributes" -key=attribute.id
+					.attribute.center
+						span.name {{ attribute.name }}
+						div.sort.column.fill-h.disabled
+							# clickable html element todo
+							span.sort-up %click="toggleSortDirection(attribute.id, 1)" -class="{highlighted: sortersByAttribute[attribute.id].direction===1}"
+								| ⮝
+							span.sort-down %click="toggleSortDirection(attribute.id, -1)" -class="{highlighted: sortersByAttribute[attribute.id].direction===-1}"
+								| ⮟
+						div.index.highlighted if="sortersByAttribute[attribute.id].index >= 0"
+							| {{ sortersByAttribute[attribute.id].index + 1 }}
 		tbody
 			tr v-for="entry in result" # key? todo
 				td
@@ -16,15 +25,27 @@
 
 <script lang="coffee">
 import Vue from 'vue'
-import { mapActions, mapState } from 'vuex'
+import { mapActions, mapState, mapGetters } from 'vuex'
 
 export default Vue.extend(
 	name: 'ResultView'
 	# components: { TokenInput, PromiseButton, PromiseForm, ReadMore }
-	computed: mapState('search', [
-		'attributes'
-		'result'
-	])
+	methods: {
+		...mapActions('search', [
+			
+		])
+		toggleSortDirection: (attribute, direction) ->
+			@$store.dispatch('search/toggleSortDirection', { attribute, direction })
+	}
+	computed: {
+		...mapState('search', [
+			'attributes'
+			'result'
+		])
+		...mapGetters('search', [
+			'sortersByAttribute'
+		])
+	}
 )
 </script>
 
@@ -35,9 +56,28 @@ table
 tr
 	height: 1em
 td, th
-	padding: 1.7vw
+	padding: 1.5vw
 	border-bottom: 1px solid #f1f1f1
-td.value
+.attribute
+	height: 1.3em
+	.sort
+		padding-left: 0.7vw
+		font-size: 80%
+		cursor: pointer
+		position: relative
+		.sort-up, .sort-down
+			&:hover
+				color: var(--color-main)
+		.sort-up
+			position: relative
+			top: -0.3em
+		.sort-down
+			position: absolute
+			bottom: -0.7em
+	.index
+		padding-left: 0.3vw
+		font-size: 80%
+.value
 	font-size: 80%
 
 </style>
--- web/src/store/search-store.coffee
+++ web/src/store/search-store.coffee
@@ -1,5 +1,6 @@
 import storageService from '@/services/storage-service'
 import axios from 'axios'
+import Vue from 'vue'
 
 export default
 	namespaced: true
@@ -7,21 +8,47 @@ export default
 		product: 'test'
 		result: null
 		attributes: [
-			{
-				name: 'a1'
-			},
-			{
-				name: 'a2'
-			},
-			{
+				id: 1
+				name: 'a1asdfasdf'
+			,
+				id: 2
+				name: 'a2asdfasdf'
+			,
+				id: 3
 				name: 'a3'
-			}
 		]
 		filters: null
-		sorters: null
+		sorters: [
+				attribute: 2
+				direction: 1
+			,
+				attribute: 3
+				direction: -1
+		]
+	getters:
+		sortersByAttribute: (state) -> # todo vocabulary unclear
+			state.attributes.reduce((all, attribute) =>
+				sorterIndex = state.sorters.findIndex((sorter) => sorter.attribute == attribute.id)
+				if sorterIndex > -1
+					all[attribute.id] =
+						index: sorterIndex
+						direction: state.sorters[sorterIndex].direction
+				else
+					all[attribute.id] = {}
+				return all
+			, {})
 	mutations:
+		removeSorterAt: (state, index) -> Vue.delete(state.sorters, index)
+		addSorter: (state, sorter) -> state.sorters.push(sorter)
 		setResult: (state, result) -> state.result = result
 	actions:
+		toggleSortDirection: ({ commit, state, getters }, { attribute, direction }) ->
+			sorter = getters.sortersByAttribute[attribute]
+			if sorter
+				commit('removeSorterAt', sorter.index)
+				if sorter.direction == direction
+					return
+			commit('addSorter', { attribute, direction })
 		search: ({ commit, state }) ->
 			if state.result
 				return
