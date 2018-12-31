e9dc18c08aec7f6e4abff9c38c90bce369906058
Philip Waritschlager <philipwaritschlager@web.de>
2018-12-31 02:56:49 +0100

    filters styling

--- web/src/components/result-view/result-table/Filters.vue
+++ web/src/components/result-view/result-table/Filters.vue
@@ -1,21 +1,19 @@
 <template lang="slm">
 # :_='
 div
-	div.filters
-		div.filter.padding.margin each=filter
-			strong  $conditionById[filter.condition].long
-			span if=filter.conditionValue
-				|  $filter.conditionValue
-			button @click=removeFilter(filter) 🗙
-	div.center
-		button if=!showForm @click=showForm=true
+	div.filters.justify-center
+		div.filter each=filter
+			span $conditionById[filter.condition].long
+			strong if=filter.conditionValue
+				| &nbsp; $filter.conditionValue
+			button @click=removeFilter(filter) × # 🗙
+		button.add.btn.fade-in if=!showForm @click=showForm=true
 			| +
-		button else @click=showForm=false
-			| -
-		div.center.margin if=showForm
-			promise-form#form.box.padding-xl button-label="Add" :action=addFilter button-float-right
+	div.center.column
+		popup if=showForm @close=showForm=false
+			promise-form#form button-label="Add" :action=addFilter button-float-right
 				div.flex
-					div.attribute-select.padding if=!attributeId
+					div.attribute-select.padding if=!attributeId # todo currently unused
 						label.column
 							| Attribute
 							attribute-select name=attributeId required :attribute-ids=attributeIds
@@ -74,6 +72,7 @@ export default Vue.extend(
 				id: 'nn'
 				abbr: '&excl;&#8709;'
 				long: 'not empty'
+			# todo: contains
 		]
 	methods: {
 		...mapActions('search', [
@@ -105,20 +104,23 @@ export default Vue.extend(
 </script>
 
 <style lang="stylus" scoped>
-.filter
-	background: lightgrey
+.filters
+	flex-wrap: wrap
+	font-size: 80%
+	.filter, .add
+		margin: 3px
+		padding: 4px
+	.filter
+		background: var(--color-main)
 #form
 	max-width: 750px
 	.attribute-select, .condition-value
-		flex: 2
+		width: 155px
 	.condition
-		flex: 1
-		min-width: 100px
-		label
-			text-align: center
 		select
 			width: 62px
 			font-family: monospace
+			margin: 0 auto
 	.condition-value
 		input
 			width: 130px
