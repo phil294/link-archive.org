e12cc6ec4a0dcdb4eb4fb1376b022cb3fd366bfc
phi <philip@waritschlager.de>
2019-07-18 22:53:35 +0200

    [up] slight header rework & externalize global styles

--- web/build/custom-loaders/slm-loader.js
+++ web/build/custom-loaders/slm-loader.js
@@ -2,13 +2,11 @@
   module.exports = function(slmdoc) {
     var keyword, replaces, rule, standalone_keywords;
     // Allow inline comments: # followed by whitespace
-    slmdoc = slmdoc.replace(/# .*/g, '');
-    // Allow ` instead of "
-    
     // Allow attributes without quotes syntax. E.g: input @click=my_method
-    slmdoc = slmdoc.replace(/`/g, '"').replace(/(?<=\s[a-zA-Z.@%:_-]+=)([^\s"]+)(?=\s|$)/g, '"$1"'); // [WS]%src= // Add quotes
+    slmdoc = slmdoc.replace(/# .*/g, '').replace(/(?<=\s[a-zA-Z.@%#:_-]+=)([^\s"]+)(?=\s|$)/g, '"$1"'); // [WS]%src= // Add quotes
     // my_src				<- captured
     // [WS]
+    
     // Things to replace
     replaces = [
       [/(?<=\s)if="/g,
@@ -46,17 +44,20 @@
       ],
       // :class.myclass="condition" to :class="{'myclass':condition}"
       [/(?<=\s:class)\.([\w-]+)="([^"]+)"(?=\s)/g,
-      '="{\'$1\':$2}"']
+      '="{\'$1\':$2}"'],
+      [
+        /(?<=\s)#(.+)(?=\s)/g,
+        'v-slot:$1' // this is in fact supported by vue itself natively already, but slm loader does not like it. so this one is more of a polyfill
+      ]
     ];
     for (rule of replaces) {
       slmdoc = slmdoc.replace(rule[0], rule[1]);
     }
     // Keywords that should allowed to be followed and preceded by whitespace without anything else. This has the potential to break plain text horribly
-    standalone_keywords = ['v-else', 'required', 'disabled', 'draggable', 'selected', 'exact', 'drag', 'drop', 'button_float_right'];
+    standalone_keywords = ['v-else', 'required', 'disabled', 'draggable', 'selected', 'exact', 'drag', 'drop', 'button_float_right', 'v-slot:\\w+'];
     for (keyword of standalone_keywords) {
       slmdoc = slmdoc.replace(new RegExp('(?<=\\s)(' + keyword + ')(?=\\s|$)', 'g'), '$1=""'); // [WS] // The keyword		<- captured # todo coffee supports vars in regexes // [WS]
     }
-    //console.log slmdoc
     return slmdoc;
   };
 
