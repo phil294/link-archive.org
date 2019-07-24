65d7af0b5910bdd876d801eb628247e046ecc2da
phi <philip@waritschlager.de>
2019-07-25 00:47:56 +0200

    add basic ssr data prefetch logic example into index route

--- web/src/components/Index.vue
+++ web/src/components/Index.vue
@@ -5,6 +5,16 @@ div INDEX
 <script lang="coffee">
 export default Vue.extend
 	name: 'Index'
-	async_data: ({ store }) ->
-		# store.dispatch 'test'
+	serverPrefetch: -> # note: docs say: You may find the same fetchItem() logic repeated multiple times (in serverPrefetch, mounted and watch callbacks) in each component - it is recommended to create your own abstraction (e.g. a mixin or a plugin) to simplify such code.
+		@fetch_index_data() # axios networking error handler doesnt display ssr: it modifies another component; this serverPrefetch only cares for this very component. Thus, serverPrefetch errors must (and semantically also should) be handled individually
+		# In this case: throw (do not catch)
+	methods:
+		fetch_index_data: ->
+			# @$store.dispatch('index_action'),
+	computed: {
+		table_data_fetched: -> true # this needs to determined by store as actual data is disregarded from ssr->client
+	}
+	mounted: ->
+		if !@table_data_fetched
+			@fetch_index_data()
 </script>
