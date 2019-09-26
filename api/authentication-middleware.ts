455c41dc22d39ac1ef384260c16f8a0ecd172a51
phil294 <philip@waritschlager.de>
2019-09-26 21:19:25 +0200

    [up] put user finding/creating from User into auth middleware

--- api/authentication-middleware.ts
+++ api/authentication-middleware.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import { UNAUTHORIZED } from 'http-status-codes';
+import { INTERNAL_SERVER_ERROR, UNAUTHORIZED } from 'http-status-codes';
 import { User } from './models/User';
 import TokenService from './services/TokenService';
 
@@ -13,11 +13,34 @@ export default (token_service: TokenService) =>
             } catch (error) {
                 return res.status(UNAUTHORIZED).send(error);
             }
-            const user = await User.findOne({ email: payload.email });
-            if (!user)
-                return res.status(UNAUTHORIZED).send('User not found');
-            if (payload.iat < user.min_iat)
-                return res.status(UNAUTHORIZED).send('The token expired'); // min_iat had been set to disallow the given token -> All tokens had been invalidated -> Expired -> Unauthorized
+
+            let user_optional: User | undefined;
+            // external
+            if (payload.external_type && payload.external_identifier) {
+                user_optional = await User.findOne({
+                    external_identifier: payload.external_identifier,
+                    external_type: payload.external_type,
+                });
+            // local (email)
+            } else if (payload.email) {
+                user_optional = await User.findOne({
+                    email: payload.email,
+                });
+            } else {
+                throw new Error(`${INTERNAL_SERVER_ERROR}`); // 'payload not identifiable' todo can this ever happen? would mean an invalid token generation somewhere
+            }
+            let user: User;
+            if (!user_optional) {
+                // this is a first valid token login. create account
+                user = new User(payload.user);
+                await user.save();
+            } else {
+                user = user_optional;
+                if (payload.iat < user.min_iat) {
+                    // min_iat had been set to disallow the given token -> All tokens had been invalidated -> Expired -> Unauthorized
+                    throw new Error('The token expired');
+                }
+            }
             res.locals.user = user;
         } else {
             // No user specified. This is fine - functionality that requires authentication must include user-secured or admin-secured which will then handle the respective errors appropriately.
