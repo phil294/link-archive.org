0e82804ef960f7d8ae7820e4aa6880f40d8bc87e
phil294 <philip@waritschlager.de>
2020-07-23 20:31:37 +0200

    [up] Add FormField helper component
    (cherry picked from commit d0631ce94d0bf8453510e53f7d37108b0796adb1)

---
+++ web/src/components/FormField.vue
@@ -0,0 +1,33 @@
+<template lang="slm">
+div.column :class.padding-s=!nolabel
+	label v-if=!nolabel :title=field.title :for=id
+		| $field.label
+	input :id=id v-if=!field.options v-model=model :name=field.name :type=field.type :required="field.required||!field.optional" :placeholder="field.placeholder||field.label" :maxlength=field.maxlength :step=field.step :min=field.min :max=field.max
+	select v-else="" :id=id v-model=model :name=field.name :required="field.required||!field.optional" :placeholder="field.placeholder||field.label"
+		option v-for="option of field.options" :value=option.value
+			| $option.name
+</template>
+
+<script lang="coffee">
+import emitting_model from '@/mixins/EmittingModel'
+export default
+	mixins: [ emitting_model ]
+	props:
+		default_values:
+			default: => {}
+			type: Object
+		field:
+			required: true
+			type: Object
+		nolabel:
+			type: Boolean
+			default: false
+	mounted: ->
+		@model = @default_values[@field.name]
+	computed:
+		id: ->
+			'_form_' + (@default_values.id or '') + @field.name
+</script>
+
+<style lang="stylus" scoped>
+</style>
