a12b8ccff79fda546ea6a281393467743d5728f8
phil294 <philip@waritschlager.de>
2020-07-23 16:03:27 +0200

    Add helper script to cherry-pick all "[up]" commits from downstream (partly non-public) remotes
    Interactively, one by one, need to commit, resolve etc by hand

---
+++ interactive-cherrypick-downstreams.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# should be d-1 because the very first commit might be omitted otherwise (?)
+since="2020-04-11"
+
+# git fetch --all
+
+hashes=$(git log --since="$since" --grep='\[up\]' --branches='*' --remotes='*' --reverse --no-notes --pretty='format:%H')
+while read -r hash; do
+    echo $hash
+    git cherry-pick --no-commit "$hash" ||:
+    read -r -n 1 -s -u 3 -p 'Press any key to continue. . .'
+    echo
+done 3<&0 <<<"$hashes"
