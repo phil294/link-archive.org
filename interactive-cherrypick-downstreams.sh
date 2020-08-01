41a4be92d894ffad1e74ebe3009272953454ebad
phil294 <philip@waritschlager.de>
2020-08-01 15:59:14 +0200

    reactive git fetch all

--- interactive-cherrypick-downstreams.sh
+++ interactive-cherrypick-downstreams.sh
@@ -2,13 +2,14 @@
 set -e
 
 # should be d-1 because the very first commit might be omitted otherwise (?)
-since="2020-04-11"
+since="2020-07-23"
 
-# git fetch --all
+git fetch --all
 
 hashes=$(git log --since="$since" --grep='\[up\]' --branches='*' --remotes='*' --reverse --no-notes --pretty='format:%H')
 while read -r hash; do
     echo $hash
+    # todo remove the [up] part so that they dont find their way back in to this repository recursively (?) probably not really an issue due to the "since" date
     git cherry-pick --no-commit -x "$hash" ||:
     read -r -n 1 -s -u 3 -p 'Press any key to continue. . .'
     sleep 0.5
