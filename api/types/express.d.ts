aa6751f0716c591d63af446e7d3adff1a18ee3ff
phil294 <philip@waritschlager.de>
2020-08-02 12:32:42 +0200

    remove express type response locals override

--- api/types/express.d.ts
+++ api/types/express.d.ts
@@ -4,13 +4,15 @@ interface Locals {
     user: User;
 }
 
+/* TODO whatsup
 declare module 'express' {
-    /** TODO: For some reason, this doesnt fix the Response object *without*
-     * explicitly specifying `res: Response` in the request handler.
-     * How to do this? stackoverflow.com/questions/55362741
-     * Once this is fixed, all res.locals type casts in the codebase can be
-     * removed */
+    // For some reason, this doesnt fix the Response object *without*
+    // explicitly specifying `res: Response` in the request handler.
+    // How to do this? stackoverflow.com/questions/55362741
+    // Once this is fixed, all res.locals type casts in the codebase can be
+    // removed
     export interface Response  {
         locals: Locals;
     }
 }
+// */
