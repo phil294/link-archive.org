45ef72e2b7c57fd3a1dc359a5b88062bdae0c541
phil294 <philip@waritschlager.de>
2020-10-31 21:14:43 +0100

    Remove material-icons from confirm

--- web/src/views/Confirm.vue
+++ web/src/views/Confirm.vue
@@ -3,10 +3,8 @@ popup.column.center v-if=confirm_prompt @close=confirm_answer(false)
 	.prompt.center $confirm_prompt
 	.actions.row.align-center
 		button.confirm.btn @click=confirm_answer(true)
-			i.material-icons check 
 			| Confirm
 		button.cancel.btn.btn-2 @click=confirm_answer(false)
-			i.material-icons close 
 			| Cancel
 </template>
 
