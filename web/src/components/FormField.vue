8de6b16ab819e5a30b331a8d7237947f28aa3995
phil294 <philip@waritschlager.de>
2021-01-19 15:30:38 +0100

    [up] Accept form props as direct props too
    (cherry picked from commit 8e95d6d47fc4da85ad1496861b3793aba13bb917)

--- web/src/components/FormField.vue
+++ web/src/components/FormField.vue
@@ -1,15 +1,15 @@
 <template lang="slm">
 .form-field.column :class.padding-s=!nolabel
-	label v-if=!nolabel :title=field.title :for=id
-		| $field.label
-	select v-if="field.options||field.optgroups" :id=id v-model=model :name=field.name :required="field.required||!field.optional" :placeholder="field.placeholder||field.label"
-		optgroup v-for="optgroup of field.optgroups" :label=optgroup.label
+	label v-if=!nolabel :title=fielddata.title :for=id
+		| $fielddata.label
+	select v-if="fielddata.options||fielddata.optgroups" :id=id v-model=model :name=fielddata.name :required="fielddata.required||!fielddata.optional" :placeholder="fielddata.placeholder||fielddata.label"
+		optgroup v-for="optgroup of fielddata.optgroups" :label=optgroup.label
 			/ kinda duplicate, not so pretty :-/
 			option v-for="option of optgroup.options" :value=option.value
 				| $option.name
-		option v-for="option of field.options" :value=option.value
+		option v-for="option of fielddata.options" :value=option.value
 			| $option.name
-	input :id=id v-else="" v-model=model :name=field.name :type=field.type :required="field.required||!field.optional" :placeholder="field.placeholder||field.label" :maxlength=field.maxlength :step=field.step :min=field.min :max=field.max onfocus=select()
+	input :id=id v-else="" v-model=model :name=fielddata.name :type=fielddata.type :required="fielddata.required||!fielddata.optional" :placeholder="fielddata.placeholder||fielddata.label" :maxlength=fielddata.maxlength :step=fielddata.step :min=fielddata.min :max=fielddata.max onfocus=select()
 </template>
 
 <script lang="coffee">
@@ -18,22 +18,22 @@ id_i = 0
 import emitting_model from '@/mixins/EmittingModel'
 export default
 	mixins: [ emitting_model ]
+	inheritAttrs: false
 	props:
 		default_values:
 			default: => {}
-			type: Object
-		field:
-			required: true
 			type: Object
 		nolabel:
 			type: Boolean
 			default: false
 	mounted: ->
 		if not @model
-			@model = @field.default_value ? @default_values[@field.name]
+			@model = @fielddata.default_value ? @default_values[@fielddata.name]
 	computed:
+		fielddata: ->
+			{ ...@field, ...@$attrs }
 		id: ->
-			"_form_#{@default_values.id or ''}_#{++id_i}_#{@field.name}"
+			"_form_#{@default_values.id or ''}_#{++id_i}_#{@fielddata.name}"
 </script>
 
 <style lang="stylus" scoped>
