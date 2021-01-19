7e1335305e02d90b7cb3bf0f8b6bcc189b780a0e
phil294 <philip@waritschlager.de>
2021-01-19 16:01:54 +0100

    Echo commit details and originating branch in cherrypick tool

--- interactive-cherrypick-downstreams.sh
+++ interactive-cherrypick-downstreams.sh
@@ -10,6 +10,8 @@ git fetch --all
 hashes=$(git log --since="$since" --grep='\[up\]' --branches='*' --remotes='*' --reverse --no-notes --pretty='format:%H')
 while read -r hash; do
     echo $hash
+    git show -s --format=medium $hash
+    git branch -a --contains $hash
     # todo remove the [up] part so that they dont find their way back in to this repository recursively (?) probably not really an issue due to the "since" date
     git cherry-pick --no-commit -x "$hash" ||:
     read -r -n 1 -s -u 3 -p 'Press any key to continue. . .'
