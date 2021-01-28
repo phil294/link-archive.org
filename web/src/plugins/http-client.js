776ec880420fc3a46ab50c53359bc29668731053
phil294 <philip@waritschlager.de>
2021-01-29 00:24:00 +0100

    [up] Account for unexpected axios errors and improve axios stack trace and push axios requests into store history
    (cherry picked from commit 1d85a42e93fb3a4ccf9018b69f113e9ae7655255)

--- web/src/plugins/http-client.js
+++ web/src/plugins/http-client.js
@@ -8,9 +8,15 @@ export default {
 
 		http_client.interceptors.request.use(config => {
 			store.dispatch('server_reachable')
+			const { method, url, params } = config
+			/* TODO: Remove once https://github.com/axios/axios/issues/3606 is fixed and move to response interceptor error handling below instad: */
+			store.commit('push_store_history', { method, url, params })
 			const token = store.state.session.token
 			if(token)
 				config.headers.Authorization = `Bearer ${token}`
+			/* TODO: Remove setting error_content and its references once https://github.com/axios/axios/issues/2387 is fixed: */
+			// @ts-ignore
+			config.error_context = new Error("Thrown at:")
 			return config
 		})
 
@@ -19,14 +25,18 @@ export default {
 			if (resp) {
 				let data = resp.data || resp.statusText || 'unknown error message';
 				const http_query_url = `${axios_error.config.baseURL}/${axios_error.config.url}`;
-				if(typeof data == 'object')
+				if(typeof data == 'object') {
 					data.http_query_url = http_query_url;
-				else
+				} else {
 					data += ` --- http_query_url: ${http_query_url}`
+				}
+				// Can use this for better stack trace:
+				// axios_error.config?.error_context?.stack
 				return error(data, resp.status)
-			} else {
+			} else if(axios_error.isAxiosError) {
 				return error({ status: 0 }, 0)
 			}
+			return Promise.reject(axios_error);
 		})
 
 		inject('http', http_client)
