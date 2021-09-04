729fb5cfebb13bc4c4fa27662f1c3d556a94d6bd
phil294 <philip@waritschlager.de>
2021-09-05 01:08:10 +0200

    [up] Fix autoexpanding-textarea
    it was pretty much broken and should now work 100% as expected
    
    (cherry picked from commit e5c5a5f3408864ff4153f180327d33b7c183961b)

--- web/src/components/AutoexpandingTextarea.vue
+++ web/src/components/AutoexpandingTextarea.vue
@@ -1,5 +1,5 @@
 <template lang="slm">
-textarea :name=name rows=1 :maxlength=maxlength :required=required :placeholder=placeholder model=model @focus=on_focus @blur=on_blur ref=ref :style=style
+textarea :name=name rows=1 :maxlength=maxlength :required=required :placeholder=placeholder v-model=model @focus=on_focus @blur=on_blur ref=ref :style=style v-observe-visibility="{callback:on_first_visible,once:true}"
 </template>
 
 <script lang="coffee">
@@ -18,28 +18,37 @@ export default
 			type: String
 			default: ''
 	mounted: ->
-		await @update_content_height()
-		@height = @content_height
 		await @on_blur()
 	data: ->
 		height: '100%'
 		content_height: 0
 		focussed: false
 	methods:
+		on_first_visible: (visible) ->
+			if visible
+				# This can happen and this is all necessary when this component (noshrink=true)
+				# is somewhere initially hidden via v-show=false, the scrollheight is falsely 0
+				if @content_height == "0px"
+					@update_content_height()
 		on_focus: ->
-			@height = @content_height
 			@focussed = true
+			await @update_content_height()
+			@focussed_content = @model
 		on_blur: ->
+			await @update_content_height()
 			await @$nextTick()
 			if not @noshrink
 				# shrink to fit container. else: keep showing entire content height
 				@height = '100%'
 			@focussed = false
 		update_content_height: ->
-			@height = ''
+			# Determine scroll height (aka height based on content)
+			@height = '1px' # stackoverflow.com/a/3341669/3779853
 			await @$nextTick()
 			if @$refs.ref # idk, was buggy
 				@content_height = @$refs.ref.scrollHeight + 'px'
+				if @focussed or @noshrink
+					@height = @content_height
 	computed:
 		style: ->
 			height: @height
@@ -47,12 +56,20 @@ export default
 	watch:
 		model: ->
 			@update_content_height()
-			if @focussed
-				@height = @content_height
 </script>
 
 <style lang="stylus" scoped>
 textarea
 	resize none
-	overflow hidden
+	// same as textarea height in global.stylus
+	min-height 28.6875px
+	line-height 1.1em
+	padding 6px 10px 0 // padding-bottom:0 bc ff (stackoverflow.com/questions/24923424)
+
+	// Allow scrolling but hide the scrollbar
+	overflow auto
+	-ms-overflow-style none
+	scrollbar-width none
+	&::-webkit-scrollbar
+		display none
 </style>
