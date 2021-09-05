cefef6f199a28c7f0de69e1ec4e4d92e62aad297
phil294 <philip@waritschlager.de>
2021-09-05 02:01:28 +0200

    [up] Put axios request info into error report as well
    not only as store history element
    
    (cherry picked from commit 260044a5d49636f118f23e33321c58f0b77a619b)

--- web/src/plugins/http-client.js
+++ web/src/plugins/http-client.js
@@ -1,3 +1,4 @@
+//@ts-check
 import axios from 'axios'
 
 export default {
@@ -9,14 +10,12 @@ export default {
 		http_client.interceptors.request.use(config => {
 			// Not a good idea as it will reset any present error displays
 			// store.dispatch 'server_reachable'
-			const { method, url, params } = config
-			const data = (() => {
-				try {
-					return JSON.stringify(config.data)
-				} catch(e){}
-			})()
-			/* TODO: Remove once https://github.com/axios/axios/issues/3606 is fixed and move to response interceptor error handling below instad: */
-			store.commit('push_store_history', { method, url, params, data })
+			store.commit('increase_loading_counter')
+			/* TODO: Remove once https://github.com/axios/axios/issues/3606 is fixed and move to response interceptor error handling below instead: */
+			const { method, url, params, data } = config
+			const axios_request_info = { method, url, params, data }
+			store.commit('push_store_history', axios_request_info)
+			config.axios_request_info = axios_request_info
 			const token = store.state.session.token
 			if(token)
 				config.headers.Authorization = `Bearer ${token}`
@@ -26,23 +25,33 @@ export default {
 			return config
 		})
 
-		http_client.interceptors.response.use((response => response), (axios_error) => {
-			const resp = axios_error.response;
-			if (resp) {
-				let data = resp.data || resp.statusText || 'unknown error message';
-				const http_query_url = `${axios_error.config.baseURL}/${axios_error.config.url}`;
-				if(typeof data == 'object') {
-					data.http_query_url = http_query_url;
-				} else {
-					data += ` --- http_query_url: ${http_query_url}`
-				}
-				// Can use this for better stack trace:
-				// axios_error.config?.error_context?.stack
-				return error(data, resp.status)
-			} else if(axios_error.isAxiosError) {
-				return error({ status: 0 }, 0)
+		http_client.interceptors.response.use((response => {
+			store.commit('decrease_loading_counter')
+			return response
+		}), (error) => {
+			const formatted_error = {
+				///////// s.a.
+				original_stack: error.config?.error_context?.stack,
+				axios_request_info: error.config?.axios_request_info,
+				/////////
+				stack: error.stack,
+				message: error.message,
+				data: error.response?.data || error.response?.statusText || null,
+				status: error.response?.status || 0
 			}
-			return Promise.reject(axios_error);
+			if(error.response?.status == 401)
+				store.dispatch('session/logout')
+			else if(error.response?.status == 409)
+				formatted_error.data = 'Error 409 CONFLICT – The request could not be completed because it would result in an unexpected data conflict (duplicate name, missing reference or the like).'
+			else if((error.response === undefined || error.code == 'ECONNABORTED') && !formatted_error.stack) {
+				// not really necessary as the exception will lead to
+				// error_handler using set_global_error_message anyway.
+				// also see note on 'server_reachable' above.
+				// store.dispatch 'server_unreachable'
+				formatted_error.data = 'Cannot reach server'
+			}
+			// todo class-validation errors: format properly
+			return Promise.reject(formatted_error)
 		})
 
 		inject('http', http_client)
