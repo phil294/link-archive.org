90f34be904a4c153c9298b4a494a24ad7713b6b6
phil294 <philip@waritschlager.de>
2021-09-05 14:46:04 +0200

    [up] Treat optional empty string "" as truthy
    (cherry picked from commit 91fddb3b2d7b0323a923bd598685177eacb88068)

--- web/src/components/FormField.vue
+++ web/src/components/FormField.vue
@@ -2,7 +2,7 @@
 .form-field.column :class.padding-s=!nolabel
 	label v-if=!nolabel :title=fielddata.title :for=id :disabled=fielddata.disabled
 		| $fielddata.label
-	select v-if="fielddata.options||fielddata.optgroups" :id=id v-model=model v-bind=fielddata :required="fielddata.required||!fielddata.optional" :placeholder="fielddata.placeholder||fielddata.label"
+	select v-if="fielddata.options||fielddata.optgroups" :id=id v-model=model v-bind=fielddata
 		optgroup v-for="optgroup of fielddata.optgroups" :label=optgroup.label
 			/ kinda duplicate, not so pretty :-/
 			option v-for="option of optgroup.options" :value=option.value
@@ -10,7 +10,7 @@
 		option v-for="option of fielddata.options" :value=option.value
 			| {{ option.name || option.value }}
 	slot v-else=""
-		input :id=id v-model=model v-bind=fielddata @keypress="$emit('keypress',$event)" :required="fielddata.required||!fielddata.optional" :placeholder="fielddata.placeholder||fielddata.label" onfocus=select()
+		input :id=id v-model=model v-bind=fielddata @keypress="$emit('keypress',$event)" onfocus=select()
 </template>
 
 <script lang="coffee">
@@ -38,13 +38,17 @@ export default
 		if not @model?
 			@model = @fielddata.default_value ? @default_values[@fielddata.name]
 	computed:
-		fielddata: -> {
-			# When passed as a $prop, name is somehow ignored, thats why it is
-			# listed extra here
-			name: @name
-			...@field
-			...@$attrs
-		}
+		fielddata: ->
+			data = {
+				# When passed as a $prop, name is somehow ignored, thats why it is
+				# listed extra here
+				name: @name
+				...@field
+				...@$attrs
+			}
+			data.required = data.required or not data.optional?
+			data.placeholder = data.placeholder or data.label
+			data
 		id: ->
 			"_form_#{++id_i}_#{@default_values.id or ''}_#{@fielddata.name or ''}"
 </script>
