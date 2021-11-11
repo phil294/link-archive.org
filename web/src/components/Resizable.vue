fb950888e6ad0a97842bf232745db8e9e7c6528f
phil294 <philip@waritschlager.de>
2021-11-11 23:19:47 +0100

    [up] enable Resizable on mobile devices
    (cherry picked from commit c977d0a55a89843a5d7a7e8653443100424a2ef8)

--- web/src/components/Resizable.vue
+++ web/src/components/Resizable.vue
@@ -1,7 +1,7 @@
 <template lang="slm">
 .resizable ref=resizable_ref
 	slot
-	.resize v-for="direction of ['n','e','s','w']" :class=direction @mousedown="start_resize(direction,$event)"
+	.resize v-for="direction of ['n','e','s','w']" :class=direction @mousedown="start_resize(direction,$event)" @touchstart="start_resize(direction,$event)"
 </template>
 
 <script lang="coffee">
@@ -14,20 +14,26 @@ export default
 		dimensions_start: {}
 	methods:
 		start_resize: (direction, event) ->
+			event.preventDefault()
 			@$emit 'start_resize'
 			@direction = direction
 			@offset = 0
 			# This code is similar to moveable.coffee and it would have become a v-directive
 			# if the extra .resize html elements weren't necessary
-			@mouse_start = { x: event.pageX, y: event.pageY }
+			@mouse_start = { x: event.pageX or event.changedTouches[0].pageX, y: event.pageY or event.changedTouches[0].pageY }
 			@dimensions_start =
 				width: @$el.offsetWidth
 				height: @$el.offsetHeight
 			document.addEventListener 'mousemove', @do_resize
 			document.addEventListener 'mouseup', @end_resize
+			document.addEventListener 'touchmove', @do_resize
+			document.addEventListener 'touchend', @end_resize
 		end_resize: ->
+			event.preventDefault()
 			document.removeEventListener 'mousemove', @do_resize
 			document.removeEventListener 'mouseup', @end_resize
+			document.removeEventListener 'touchmove', @do_resize
+			document.removeEventListener 'touchend', @end_resize
 			@$emit 'end_resize',
 				direction: @direction
 				offset: @offset
@@ -37,19 +43,22 @@ export default
 			@$el.style.top = ''
 			@$el.style.left = ''
 		do_resize: (event) ->
+			event.preventDefault()
+			pageX = event.pageX or event.changedTouches[0].pageX
+			pageY = event.pageY or event.changedTouches[0].pageY
 			switch @direction
 				when 'n'
-					@offset = event.pageY - @mouse_start.y
+					@offset = pageY - @mouse_start.y
 					@$el.style.top = @offset + "px"
 					@$el.style.height = @dimensions_start.height - @offset + "px"
 				when 'e'
-					@offset = event.pageX - @mouse_start.x
+					@offset = pageX - @mouse_start.x
 					@$el.style.width = @dimensions_start.width + @offset + "px"
 				when 's'
-					@offset = event.pageY - @mouse_start.y
+					@offset = pageY - @mouse_start.y
 					@$el.style.height = @dimensions_start.height + @offset + "px"
 				when 'w'
-					@offset = event.pageX - @mouse_start.x
+					@offset = pageX - @mouse_start.x
 					@$el.style.left = @offset + "px"
 					@$el.style.width = @dimensions_start.width - @offset + "px"
 </script>
