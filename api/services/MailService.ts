d0d99c94733d095d99353c01974344c7da022473
phil294 <philip@waritschlager.de>
2021-11-11 22:56:07 +0100

    [up] add optional `ignore_throttling` flag
    (and 1-2 bugfixes that were seemingly missed in this branch)
    
    (cherry picked from commit c21c6b0e2aabade38bc7e5cc15f3ddd1d2692561)

--- api/services/MailService.ts
+++ api/services/MailService.ts
@@ -18,22 +18,24 @@ class MailService {
 			},
 		})
 	}
-	public send_mail(recepient: string, subject: string, body_html: string): Promise<SentMessageInfo> {
-		// Prevent mail spam
-		const now = Date.now()
-		const time_since_last_error = now - this.last_error_time
-		this.last_error_time = now
-		if (time_since_last_error < 10000) {
-			// Last error less then 10 seconds ago
-			this.error_count_in_last_10_seconds++
-			if (this.error_count_in_last_10_seconds > 3) {
-				// More than 3 errors in 10 seconds: Do not send out any more mails
-				log('Error mail sending count limit exceeded')
-				return Promise.resolve()
+	public send_mail(recepient: string | string[], subject: string, body_html: string, ignore_throttling = false): Promise<SentMessageInfo> {
+		if(!ignore_throttling) {
+			// Prevent mail spam
+			const now = Date.now()
+			const time_since_last_error = now - this.last_error_time
+			this.last_error_time = now
+			if (time_since_last_error < 10000) {
+				// Last error less then 10 seconds ago
+				this.error_count_in_last_10_seconds++
+				if (this.error_count_in_last_10_seconds > 3) {
+					// More than 3 errors in 10 seconds: Do not send out any more mails
+					log(`Error mail sending count limit exceeded (${this.error_count_in_last_10_seconds})`)
+					return Promise.resolve(false)
+				}
+			} else {
+				// Last error at least 10 seconds ago: Reset error counter to 1 and proceed with sending out error mails from now on
+				this.error_count_in_last_10_seconds = 1
 			}
-		} else {
-			// Last error at least 10 seconds ago: Reset error counter to 1 and proceed with sending out error mails from now on
-			this.error_count_in_last_10_seconds = 1
 		}
 
 		return this.nodemailer_transport.sendMail({
