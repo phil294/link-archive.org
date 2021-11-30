2172f68682446c4fd802d66d76e56739a57ed357
phil294 <philip@waritschlager.de>
2021-11-30 01:10:23 +0100

    ignore CS TS return errors

--- coffeesense.config.js
+++ coffeesense.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   projects: [
     './web'
-  ]
+  ],
+  settings: {
+    "coffeesense.ignoredTypescriptErrorCodes": [
+      7030, // Not all code paths return a value
+      7023, // 'your_var' implicitly has return type 'any' because it does not have a return type annotation and is referenced directly or indirectly in one of its return expressions.
+    ],
+  }
 }
